2a4e76a10f87f2cc02126cd669aa61b8bb1b1105
Andrey.Prozorov <docentovich@gmail.com>
2020-03-16 00:16:15 +0300

    ✨ Add TaggedLinks.svelte component

---
+++ src/components/TaggedLinks.svelte
@@ -0,0 +1,17 @@
+<script>
+    import {onMount} from 'svelte';
+
+    let posts = [];
+
+    onMount(async function () {
+        posts = await (await fetch('http://api.localhost/wp-json/wp/v2/posts')).json();
+        console.log(posts);
+    });
+</script>
+
+<ul>
+    {#each posts as post}
+        <li>{post.title.rendered}</li>
+    {/each}
+</ul>
+
