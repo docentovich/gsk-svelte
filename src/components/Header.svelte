e1306329efc051708254b300ef90c5caeee6db2f
Andrey.Prozorov <docentovich@gmail.com>
2020-04-05 21:01:13 +0300

    ✨ base components

---
+++ src/components/Header.svelte
@@ -0,0 +1,8 @@
+<style lang="scss">
+
+</style>
+
+<header class="header">
+    Hedaer
+</header>
+
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,22 +1,36 @@
 <script>
 	import Nav from '../components/Nav.svelte';
+	import Widget from '../components/Widget.svelte';
+	import PostLinksAccordion from '../components/PostLinksAccordion.svelte';
+	import News from '../components/News.svelte';
+	import Footer from '../components/Footer.svelte';
+	import Header from '../components/Header.svelte';
 
 	export let segment;
 </script>
 
-<style>
+<style lang="scss">
 	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
+		background: url('/bg.jpg') top center no-repeat #dfdfdf;
+		background-size: 100% auto;
 	}
 </style>
 
-<Nav {segment}/>
-
 <main>
-	<slot></slot>
+	<Header></Header>
+	<Nav {segment}></Nav>
+	<div class="content">
+		<div class="sidebar">
+			<Widget>
+				<h3>Меню сайта</h3>
+				<PostLinksAccordion></PostLinksAccordion>
+			</Widget>
+			<Widget>
+				<h3>Новости</h3>
+				<News></News>
+			</Widget>
+		</div>
+		<slot></slot>
+	</div>
+	<Footer></Footer>
 </main>
