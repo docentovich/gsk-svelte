1f859dbc91cc3e5a29ccf6537d9e0ad7628f42b4
Andrey.Prozorov <docentovich@gmail.com>
2020-04-09 19:10:44 +0300

    ✨ render main page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,19 @@
+<script context="module">
+  import { apiUrlV2 } from '../helpers/constants'
+
+  export async function preload() {
+    const data = await (await this.fetch(`${apiUrlV2}/frontpage`)).json()
+
+    return { page: data }
+  }
+</script>
+
+<script>
+  export let page
+</script>
+
 <svelte:head>
-    <title>Sapper project template</title>
+  <title>Sapper project template</title>
 </svelte:head>
 <div class="content">
   {@html page.content.rendered}
