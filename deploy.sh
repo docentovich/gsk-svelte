a87855654d27a968937e9b18fd6559765646a226
Andrey.Prozorov <docentovich@gmail.com>
2020-08-11 01:02:06 +0300

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
 server="root@vm655661.vps.masterhost.tech"
-destiny="/var/www/gsk-wp/app/front"
+gsk="/var/www/gsk-wp"
+destiny="$gsk/app/front"
 src="."
 
 npm run build
-zip arch.zip __sapper__ node_modules package.json
+ssh "$server" "cd $gsk && git reset --hard && git pull"
+zip -r -q arch.zip __sapper__ node_modules package.jso
 scp -r "$src/arch.zip" "$server:/tmp/gsk/arch.zip"
-ssh "$server" "unzip /tmp/gsk/arch.zip -d $destiny/public"
+ssh "$server" "unzip /tmp/gsk/arch.zip -d $destiny/public &&
+              docker-compose -f $gsk/prod-with-node/docker-compose.yml down &&
+              docker-compose -f $gsk/prod-with-node/docker-compose.yml up -d &&
+              "
